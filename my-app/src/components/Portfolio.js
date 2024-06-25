4c7e22273c9850f6ed1008b2b3c84d4d2e9fd494
Husky Lover <ssolivares0@gmail.com>
2024-06-24 23:12:33 -0400

    Made Portfolio component, added initial styling, and imported images

--- my-app/src/components/Portfolio.js
+++ my-app/src/components/Portfolio.js
@@ -1,6 +1,13 @@
 import { Container, Row, Col } from "react-bootstrap";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+// import project images here
+import pwaTextEditor from "../assets/images/pwaTextEditor.png";
+import datasnake from "../assets/images/datasnake.png";
+import timedQuiz from "../assets/images/timedQuiz.png";
+import sqlEmployeeTracker from "../assets/images/sqlEmployeeTracker.png";
+import ormEcommerce from "../assets/images/ormEcommerce.png";
+import practiceWebpage from "../assets/images/practiceWebpage.png";
 
 export const Portfolio = () => {
     const responsive = {
@@ -29,7 +36,34 @@ export const Portfolio = () => {
                 <Row>
                     <Col>
                         <div className="skill-box">
-                            <h2></h2>
+                            <h2>Project Portfolio</h2>
+                            <p>Here are a few projects that I have created during my coding bootcamp course.<br>Click on any project to checkout the code!</br></p>
+                            <Carousel responsive={responsive} infinite={true} className="skill-slider">
+                                <div className="item">
+                                    <img src={ pwaTextEditor } alt="Image"/>
+                                    <h5>PWA Text Editor</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={ datasnake } alt="Image"/>
+                                    <h5>Data Snake</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={ timedQuiz } alt="Image"/>
+                                    <h5>Timed Quiz</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={ sqlEmployeeTracker} alt="Image"/>
+                                    <h5>SQL Employee Tracker</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={ ormEcommerce} alt="Image"/>
+                                    <h5>ORM E commerce</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={ practiceWebpage} alt="Image"/>
+                                    <h5>Practice Webpage</h5>
+                                </div>
+                            </Carousel>
                         </div>
                     </Col>
                 </Row>

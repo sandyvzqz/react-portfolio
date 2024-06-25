027fc3eb50af528c45229e853f2cb6818a265a47
Husky Lover <ssolivares0@gmail.com>
2024-06-24 23:17:49 -0400

    re-doing Portfolio component and styling

--- my-app/src/components/Portfolio.js
+++ my-app/src/components/Portfolio.js
@@ -1,6 +1,6 @@
 import { Container, Row, Col } from "react-bootstrap";
 import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
+
 // import project images here
 import pwaTextEditor from "../assets/images/pwaTextEditor.png";
 import datasnake from "../assets/images/datasnake.png";
@@ -9,65 +9,4 @@ import sqlEmployeeTracker from "../assets/images/sqlEmployeeTracker.png";
 import ormEcommerce from "../assets/images/ormEcommerce.png";
 import practiceWebpage from "../assets/images/practiceWebpage.png";
 
-export const Portfolio = () => {
-    const responsive = {
-        superLargeDesktop: {
-          // the naming can be any, depends on you.
-          breakpoint: { max: 4000, min: 3000 },
-          items: 5
-        },
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 3
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1
-        }
-      };
-    
-    return (
-        <section className="skill" id="skills">
-            <Container>
-                <Row>
-                    <Col>
-                        <div className="skill-box">
-                            <h2>Project Portfolio</h2>
-                            <p>Here are a few projects that I have created during my coding bootcamp course.<br>Click on any project to checkout the code!</br></p>
-                            <Carousel responsive={responsive} infinite={true} className="skill-slider">
-                                <div className="item">
-                                    <img src={ pwaTextEditor } alt="Image"/>
-                                    <h5>PWA Text Editor</h5>
-                                </div>
-                                <div className="item">
-                                    <img src={ datasnake } alt="Image"/>
-                                    <h5>Data Snake</h5>
-                                </div>
-                                <div className="item">
-                                    <img src={ timedQuiz } alt="Image"/>
-                                    <h5>Timed Quiz</h5>
-                                </div>
-                                <div className="item">
-                                    <img src={ sqlEmployeeTracker} alt="Image"/>
-                                    <h5>SQL Employee Tracker</h5>
-                                </div>
-                                <div className="item">
-                                    <img src={ ormEcommerce} alt="Image"/>
-                                    <h5>ORM E commerce</h5>
-                                </div>
-                                <div className="item">
-                                    <img src={ practiceWebpage} alt="Image"/>
-                                    <h5>Practice Webpage</h5>
-                                </div>
-                            </Carousel>
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-        </section>
-    )  
-};
+export const Portfolio = () => {}
